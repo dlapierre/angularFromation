2675d38cd9e6ea147dceb5c6657eb144309346ed
David LAPIERRE <david.lapierre@kleegroup.com>
2021-09-22 13:52:29 +0100

    dev: ngFor stock

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  articles: Article[] = [
+    {
+      name: 'Tournevis',
+      price: 2.99,
+      qty: 120,
+    },
+    {
+      name: 'Marteau',
+      price: 8.99,
+      qty: 150,
+    },
+    {
+      name: 'Perceuse',
+      price: 82.99,
+      qty: 15,
+    },
+    {
+      name: 'Pelle',
+      price: 15.99,
+      qty: 5,
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

96141c24f2971eedf5284835c84889a813a827b2
David LAPIERRE <david.lapierre@kleegroup.com>
2021-09-22 14:37:50 +0100

    dev: Form mapping ReactieForm

--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent implements OnInit {
+  f = new FormGroup({
+    name: new FormControl('init', [Validators.required]),
+    price: new FormControl(2.5, [Validators.required]),
+    naqtyme: new FormControl(12, [Validators.required]),
+  });
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

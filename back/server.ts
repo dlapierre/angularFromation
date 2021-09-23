699013b5938b6fafb571267172d79541e3d3e34a
David LAPIERRE <david.lapierre@kleegroup.com>
2021-09-23 14:09:03 +0100

    dev: back GET articles

--- back/server.ts
+++ back/server.ts
@@ -1,10 +1,16 @@
+import cors from "cors";
 import express from "express";
 import serveIndex from "serve-index";
-import cors from "cors";
+import { Article } from "./../front/src/app/interfaces/article";
 
 const app = express();
 const port = 3000;
 const dir = "../front/dist/front";
+
+var articles: Article[] = [
+  { name: "tournevis", price: 5.99, qty: 120 },
+  { name: "marteau", price: 9.99, qty: 130 },
+];
 
 app.use(cors());
 
@@ -13,6 +19,9 @@ app.use("/", (req, res, next) => {
   next();
 });
 
+app.get("/api/articles", (req, response) => {
+  response.json(articles);
+});
 app.use(express.static(dir));
 app.use(serveIndex(dir, { icons: true }));
 

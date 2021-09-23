d546063c4b01bb8fd49baaef51afd09f4b3bf80f
David LAPIERRE <david.lapierre@kleegroup.com>
2021-09-23 14:21:22 +0100

    dev: HTTP Get map

--- front/src/app/services/http-article.service.ts
+++ front/src/app/services/http-article.service.ts
@@ -1,6 +1,8 @@
+import { Article } from './../interfaces/article';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ArticleService } from './article.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -13,16 +15,26 @@ export class HttpArticleService extends ArticleService {
   }
 
   refresh() {
-    this.http.get('http://localhost:3000/api/articles').subscribe({
-      next: (data) => {
-        console.log(data);
-      },
-      complete: () => {
-        console.log('complete');
-      },
-      error: (error) => {
-        console.log(error);
-      },
-    });
+    this.http
+      .get<Article[]>('http://localhost:3000/api/articles')
+      .pipe(
+        map((articles) => {
+          articles.forEach((a) => (a.name = a.name.toUpperCase()));
+          return articles;
+        })
+      )
+      .subscribe({
+        next: (articles) => {
+          console.log('articles: ', articles);
+          this.articles = articles;
+          this.save();
+        },
+        complete: () => {
+          console.log('complete');
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
   }
 }

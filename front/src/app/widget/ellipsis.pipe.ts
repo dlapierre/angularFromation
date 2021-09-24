ce2602a1f235e9cc0cf960304285af85a5ec8c58
David LAPIERRE <david.lapierre@kleegroup.com>
2021-09-24 08:53:55 +0100

    dev: add new Pipe maxLenght trunck

--- front/src/app/widget/ellipsis.pipe.ts
+++ front/src/app/widget/ellipsis.pipe.ts
@@ -5,6 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class EllipsisPipe implements PipeTransform {
   transform(value: string, maxLength: number = 10): string {
+    // todo traiter undefined
     return value.length > maxLength
       ? value.substr(0, maxLength).concat('...')
       : value;

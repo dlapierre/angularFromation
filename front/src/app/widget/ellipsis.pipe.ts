1f24b6a67bf8ae87146d792eaf9886c98357b41b
David LAPIERRE <david.lapierre@kleegroup.com>
2021-09-24 08:53:19 +0100

    dev: add new Pipe maxLenght trunck

--- front/src/app/widget/ellipsis.pipe.ts
+++ front/src/app/widget/ellipsis.pipe.ts
@@ -7,8 +7,6 @@ export class EllipsisPipe implements PipeTransform {
   transform(value: string, maxLength: number = 10): string {
     return value.length > maxLength
       ? value.substr(0, maxLength).concat('...')
-      : !value
-      ? ''
       : value;
   }
 }
